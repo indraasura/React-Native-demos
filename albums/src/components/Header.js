5289ef2446f544a0db6ab0603e5ec15fb1fdb253
Swarup <swaruphegde@gmail.com>
2019-06-25 15:41:16 +0530

    fetching http using axios

---
+++ albums/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Header = props => {
+    const { textStyle, viewStyle } = styles
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums</Text>
+        </View>
+    )
+}
+
+const styles = {
+    textStyle: {
+        fontSize: 20,
+    },
+    viewStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+    }
+}
+
+export default Header
