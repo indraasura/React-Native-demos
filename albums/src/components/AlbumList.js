1d3ff45f86834d22f4c5cde3116a3eeabc839e0e
Swarup <swaruphegde@gmail.com>
2019-06-26 00:12:33 +0530

    http requests done

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import axios from 'axios'
+import AlbumDetail from './AlbumDetail'
 
 class AlbumList extends Component {
     state = {
@@ -10,10 +11,14 @@ class AlbumList extends Component {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
             .then(response => this.setState({ albums: response.data }))
     }
+    renderAlbums() {
+        return this.state.albums.map(album => 
+        <AlbumDetail key={album.title} album={album} />)
+    }
     render() {
         return (
             <View>
-                <Text>Album List Goes here</Text>
+                {this.renderAlbums}
             </View>
         )
     }
