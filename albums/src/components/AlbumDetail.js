61a90d638e2f5988c3996c22889e45b73c12b5e7
Swarup <swaruphegde@gmail.com>
2019-06-27 12:10:17 +0530

    card header section added

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,16 +1,47 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
 
-const AlbumDetail = props =>{
+const AlbumDetail = ({ album }) =>{
+    // Destrucutre props to albums, further destructre albums to individual elements 
+    // if there are multiple references
+    // General: destructure anything that appears multiple times
+
+    const { headerContainerStyle, thumbnail_style } = styles
+
+    const { title,artist, thumbnail_image } = album
+
     return (
         <Card>
              <CardSection>
-                 <Text>{props.album.title}</Text>
+                 <View>
+                     <Image
+                        // To render image, we require styling
+                        style={thumbnail_style}
+                        source={{ uri: thumbnail_image }} 
+                     />
+                 </View>
+                 <View style={headerContainerStyle}>
+                     <Text>{title}</Text>
+                     <Text>{artist}</Text>
+                 </View>
+                 
              </CardSection>
         </Card>
     )
+}
+
+const styles = {
+    headerContainerStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnail_style: {
+         height: 50,
+         width: 50,
+         marginRight: 5
+    }
 }
 
 export default AlbumDetail
