af0a4860db6f12f81babb1491898dbb90b6f3699
Swarup <swaruphegde@gmail.com>
2019-07-03 23:42:30 +0530

    final commit, everything's working

--- auth/src/App.js
+++ auth/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
     renderContent = () => {
         switch (this.state.loggedIn){
             case true:  
-                return <Button>Log Out</Button>
+                return <Button onPress={() => firebase.auth().signOut}>Log Out</Button>
             case false:
                 return <LoginForm />
             default:
